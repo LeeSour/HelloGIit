e5568574f5f58b28afc7cce4a7cec9928eaec8e7
LeeSour <LeeSour326@163.com>
2019-07-11 15:28:18 +0800

    this is version 2.0 test project

--- src/Hello.java
+++ src/Hello.java
@@ -2,9 +2,11 @@
  * @title：这是一个提交到git管理代码的测试工程
  * @author：LeeSour326@163.com
  * @time:2019-07-11 15:06
+ * miaoshu:这是我的v2.0版本
  */
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hellp,Git");
+        System.out.println("when i meet u at the first time ,i feel myself is fall in love with u");
     }
 }
