1bc21542ffd0f3aaae09818f4ba7c64cfd92428e
LeeSour <LeeSour326@163.com>
2019-07-11 15:49:19 +0800

    and create a new file name is demo

--- src/Hello.java
+++ src/Hello.java
@@ -2,11 +2,12 @@
  * @title：这是一个提交到git管理代码的测试工程
  * @author：LeeSour326@163.com
  * @time:2019-07-11 15:06
- * miaoshu:这是我的v2.0版本
+ * miaoshu:这是我的v3.0版本
  */
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hellp,Git");
         System.out.println("when i meet u at the first time ,i feel myself is fall in love with u");
+        System.out.println("I am very happy because i can use 'syso' to print the words");
     }
 }
