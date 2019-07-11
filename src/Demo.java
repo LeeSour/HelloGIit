ba3415657f19363b1ea450ea38cccef62864efa9
LeeSour <LeeSour326@163.com>
2019-07-11 15:34:45 +0800

    and create a new file name is demo

---
+++ src/Demo.java
@@ -0,0 +1,10 @@
+/**
+ * des:this is the demo v1.0
+ * time:2019-07-11
+ * author:LeeSour326@163.com
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("this is the first demo in the ide");
+    }
+}
