4926de07f39e970f02eafc4537f92de9de7cc5e7
LeeSour <LeeSour326@163.com>
2019-07-11 15:09:14 +0800

    this is version 1.0 test project

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * @title：这是一个提交到git管理代码的测试工程
+ * @author：LeeSour326@163.com
+ * @time:2019-07-11 15:06
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hellp,Git");
+    }
+}
