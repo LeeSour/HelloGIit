de1345df1b283d21e819d6bd922d72c509bb4cc6
LeeSour <LeeSour326@163.com>
2019-07-15 11:01:50 +0800

    this is a thread exchange's demo

--- src/ThreadDemo/ThreadDemo01.java
+++ src/ThreadDemo/ThreadDemo01.java
@@ -3,9 +3,10 @@ package ThreadDemo;
 public class ThreadDemo01{
 
 
-    public static int tickets = 10;
+    public static int tickets = 50;
     public static void main(String[] args) {
-
+        new Thread(new ChangeThread()).run();
+        new Thread(new ChangeThread()).run();
 
     }
 }
@@ -17,8 +18,10 @@ class ChangeThread implements Runnable{
     public void run() {
 
         while (ThreadDemo01.tickets>0){
-            ThreadDemo01.tickets--;
-            System.out.println(Thread.currentThread().getId()+"窗口，卖出一张票，剩余票数为："+ThreadDemo01.tickets);
+            synchronized (ThreadDemo01.class){
+                ThreadDemo01.tickets--;
+                System.out.println(Thread.currentThread().getId()+"窗口，卖出一张票，剩余票数为："+ThreadDemo01.tickets);
+            }
             try {
                 Thread.sleep(100);
             }catch (InterruptedException e){
