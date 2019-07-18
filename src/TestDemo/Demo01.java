9062b6d6b611c548da713d24c0b193edc325c22b
LeeSour <LeeSour326@163.com>
2019-07-18 14:16:19 +0800

    this is a get the time's demo

--- src/TestDemo/Demo01.java
+++ src/TestDemo/Demo01.java
@@ -1,9 +1,18 @@
 package TestDemo;
 
+import java.util.Calendar;
+
+/**
+ *
+ * @title: print the time the day of yesterday
+ * @author: LeeSour
+ * @time: 7-18
+ */
 public class Demo01 {
     public static void main(String[] args) {
-
-        System.out.println(123456);
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE,-1);
+        System.out.println(cal.getTime());
 
 
     }
